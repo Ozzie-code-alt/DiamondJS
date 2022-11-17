ca349ec8193f6e7ef2c79513bc791f865fe6a61c
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2022-11-17 20:57:13 +0800

    Push1

---
+++ main.js
@@ -0,0 +1,4 @@
+let num = prompt("Enter your Age")
+let years = parseInt(num) - 18;
+num >=18 ?console.log("You are old enough to drive") :console.log(` You are Left wih ${Math.abs(years)} years`)
+
