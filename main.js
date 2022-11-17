5608e30eaef53c39afc545890defbc6d761f9657
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2022-11-17 21:05:39 +0800

    Another set

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-let num = prompt("Enter your Age")
-let years = parseInt(num) - 18;
-num >=18 ?console.log("You are old enough to drive") :console.log(` You are Left wih ${Math.abs(years)} years`)
+// let num = prompt("Enter your Age")
+// let years = parseInt(num) - 18;
+// num >=18 ?console.log("You are old enough to drive") :console.log(` You are Left wih ${Math.abs(years)} years`)
 
+
+let num = prompt("Enter your age")
+let age2 = 18
+let comparison = parseInt(num) - age2
+num >18 ? console.log(`you are older by${comparison}`):console.log(`i am older by ${Math.abs(comparison)}` )
