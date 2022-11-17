f5aefc9564af1addea725590933b31e8edad898a
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2022-11-17 21:38:58 +0800

    Added Nan Error

--- main.js
+++ main.js
@@ -3,7 +3,18 @@
 // num >=18 ?console.log("You are old enough to drive") :console.log(` You are Left wih ${Math.abs(years)} years`)
 
 
-let num = prompt("Enter your age")
-let age2 = 18
-let comparison = parseInt(num) - age2
-num >18 ? console.log(`you are older by${comparison}`):console.log(`i am older by ${Math.abs(comparison)}` )
+// let num = prompt("Enter your age")
+// let age2 = 18
+// let comparison = parseInt(num) - age2
+// num >18 ? console.log(`you are older by${comparison}`):console.log(`i am older by ${Math.abs(comparison)}` )
+
+
+
+
+
+
+
+
+
+let NanErrorLmao = ("b" + "a" + + "a" + "a")
+console.log(NanErrorLmao)
---
+++ readMe.md
@@ -0,0 +1,2 @@
+#Dont Bother Checking this out, is full of NaN'n in bananas
+-im warning you
