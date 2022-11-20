ee34c0bba8f309af7d1fa712f3ba44a7bc38ea06
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2022-11-20 14:25:07 +0800

    Added Codewars Attempt 1

--- main.js
+++ main.js
@@ -9,12 +9,36 @@
 // num >18 ? console.log(`you are older by${comparison}`):console.log(`i am older by ${Math.abs(comparison)}` )
 
 
+function validatePIN(pin) {
+ 
 
+ let filter = pin.match(/\d+/g);
 
+    if(pin.length == 4 && pin == filter){
+      console.log(filter)
+      return true
+    }
+    else if(pin.length == 6 && pin == filter){
+      console.log(filter)   
+      return true
+    }
+    else{
+      return false
+    }
+     
+   }
 
+validatePIN(`accc34`)
+// let pattern = /\d+/;
+// console.log(pattern.test(`Sally is 1 year old. 
+// Harry's phone number is 1234.`));
+// console.log(pattern.test("I had frozen yoghurt after lunch today"));
+// function printThis(pin){
+//    let pinned= /\d+/;
+//    console.log(pinned.test(pin));
+//    console.log(pinned.test(pin));
+// }
+// printThis(`ABC123343`)
 
-
-
-
-let NanErrorLmao = ("b" + "a" + + "a" + "a")
-console.log(NanErrorLmao)
+// let NanErrorLmao = ("b" + "a" + + "a" + "a")
+// console.log(NanErrorLmao)
