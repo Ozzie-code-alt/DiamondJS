a98b9f107f56171b7c24fcafeb58af94f8c57193
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2023-03-11 02:02:40 +0800

    Hello march 11

--- main.js
+++ main.js
@@ -459,6 +459,8 @@
 
 /*You're a square!*/
 
-var isSquare = function(n){
-   return Math.sqrt(n) % 1 === 0 ? true : false
- }
+// var isSquare = function(n){
+//    return Math.sqrt(n) % 1 === 0 ? true : false
+//  }
+
+/*Grasshopper - Messi goals function*/
