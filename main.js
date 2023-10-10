94457e5697f868e592a4cd244bb8c5b143e6f994
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2023-10-10 22:32:57 +0800

    hehe

--- main.js
+++ main.js
@@ -1198,7 +1198,7 @@
 // }
 
 // sorting buuble sort and built in sort func 
-function solution(nums) {
+// function solution(nums) {
   //   if (nums == null) {
   //     return [];
   //   } else if (nums.length === 0) {
@@ -1228,3 +1228,33 @@ function solution(nums) {
   // }
 
   //work ples now work ples 
+
+
+  // multiple of 5 
+  function roundToNext5(n) {
+    // Calculate the remainder when dividing by 5
+    const remainder = Math.abs(n) % 5;
+  
+    // If the remainder is 0, n is already a multiple of 5
+    if (remainder === 0) {
+      return n;
+    }
+  
+    // Calculate the difference between 5 and the remainder
+    const difference = 5 - remainder;
+    console.log(difference)
+    // Determine the sign of the result based on the sign of n
+    const sign = n >= 0 ? 1 : -1;
+  
+    // Add the difference to n with the appropriate sign
+  const heheSign =   n + difference * sign;
+    if(n == -1){
+      return 0
+    }
+    else if (n < 0){
+      return heheSign +5
+    }
+    else {
+      return heheSign
+    }
+  }
